8740ab31d950b2c31267d6b984459994878823c5
kenjovan <kenjovan55@gmail.com>
2024-04-26 11:39:30 +0700

    Login and Register Basic

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, Image, Pressable, SafeAreaView, StyleSheet, Switch, Text, TextInput, View, ScrollView } from 'react-native'
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from "./Login"
+import Register from './Register';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Login')
+        }
+      />
+    </SafeAreaView>
+
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+const getFullName = (firstName, secondName, thirdName) => {
+  return firstName + ' ' + secondName + ' ' + thirdName;
+};
+
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
+  return (
+    <View style={{ padding: 10 }}>
+      <TextInput
+        style={{ height: 40 }}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{ padding: 10, fontSize: 42 }}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TestButton = props => {
+  const [isHungry, setIsHungry] = useState(true);
+
+  return (
+    <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+      />
+    </View>
+  );
+};
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }}/>
+        <Stack.Screen name="Register" component={Register} options={{ title: 'Register' }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+};
+
+export default App;
---
+++ Register.js
@@ -0,0 +1,227 @@
+import React, { useState } from 'react';
+import { View, Text, Image, ScrollView, TextInput, SafeAreaView, StyleSheet, Pressable, TouchableOpacity } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import "@fontsource/poppins/700-italic.css"; 
+
+const Register = () => {
+    const [emailActive, setEmailActive] = useState(false);
+    const [passwordActive, setPasswordActive] = useState(false);
+
+    const handleEmailFocus = () => {
+        setEmailActive(true);
+    };
+
+    const handleEmailBlur = () => {
+        setEmailActive(false);
+    };
+
+    const handlePasswordFocus = () => {
+        setPasswordActive(true);
+    };
+
+    const handlePasswordBlur = () => {
+        setPasswordActive(false);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.titleContainer}>
+                <Text
+                    style={styles.title}>Login here</Text>
+                <Text
+                    style={styles.subTitle}>Welcome back you've been missed!</Text>
+            </View>
+
+            <View style={styles.textInputContainer}>
+                <TouchableOpacity onPress={handleEmailFocus}>
+                    <TextInput
+                        onFocus={handleEmailFocus}
+                        onBlur={handleEmailBlur}
+                        style={[styles.textInput, emailActive && styles.textInputFocus]}
+                        placeholder="Email"
+                        placeholderTextColor='#626262'
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handlePasswordFocus}>
+                    <TextInput
+                        onFocus={handlePasswordFocus}
+                        onBlur={handlePasswordBlur}
+                        style={[styles.textInput, passwordActive && styles.textInputFocus]}
+                        placeholder="Password"
+                        placeholderTextColor='#626262'
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleEmailFocus}>
+                    <TextInput
+                        onFocus={handleEmailFocus}
+                        onBlur={handleEmailBlur}
+                        style={[styles.textInput, emailActive && styles.textInputFocus]}
+                        placeholder="Email"
+                        placeholderTextColor='#626262'
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handlePasswordFocus}>
+                    <TextInput
+                        onFocus={handlePasswordFocus}
+                        onBlur={handlePasswordBlur}
+                        style={[styles.textInput, passwordActive && styles.textInputFocus]}
+                        placeholder="Password"
+                        placeholderTextColor='#626262'
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => 'null'}>
+                    <Text style={styles.forgotPassword}>
+                        Forgot your password?
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.signInButton}
+                    onPress={() => 'null'}>
+                    <Text style={styles.signInTitle}>Sign up</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    onPress={() => 'null'}>
+                    <Text style={styles.createNewAccountTitle}>Create new account</Text>
+                </TouchableOpacity>
+
+                <Text style={styles.continueWithTitle}>Or continue with</Text>
+            </View>
+
+
+            {/* <View>
+                <Image
+                    source={{
+                        uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+                    }}
+                    style={{ width: 200, height: 200 }}
+                />
+
+            </View> */}
+
+        </SafeAreaView>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    titleContainer: {
+        width: 225,
+        // height: 131,
+        // borderWidth: 1,
+        alignItems: 'center',
+        alignSelf: 'center'
+        // flex:1
+
+    },
+    title: {
+        // textAlign: 'center',
+        paddingVertical: 40,
+        lineHeight: 45,
+        fontSize: 30,
+        fontWeight: '700',
+        color: '#064BB5'
+    },
+    subTitle: {
+        textAlign: 'center',
+        lineHeight: 30,
+        fontSize: 20,
+        fontWeight: '600',
+    },
+    textInputContainer: {
+        width: 357,
+        height: 157,
+        gap: 29,
+        paddingVertical: 40,
+        alignSelf: 'center'
+
+    },
+    textInput: {
+        width: 357,
+        // height: 64,
+        // top: 309,
+        // left: 16,
+        gap: 10,
+        backgroundColor: '#F1F4FF',
+        borderRadius: 10,
+        paddingTop: 20,
+        paddingRight: 35,
+        paddingBottom: 20,
+        paddingLeft: 20,
+        fontSize: 16,
+        lineHeight: 20,
+        fontWeight: '500',
+    },
+    textInputFocus: {
+        width: 357,
+        // height: 64,
+        // top: 309,
+        // left: 16,
+        gap: 10,
+        borderColor: '#064BB5',
+        backgroundColor: '#F1F4FF',
+        borderRadius: 10,
+        paddingTop: 20,
+        paddingRight: 35,
+        paddingBottom: 20,
+        paddingLeft: 20,
+        fontSize: 16,
+        lineHeight: 20,
+        fontWeight: '500',
+        borderWidth: 2
+    },
+    forgotPassword: {
+        textAlign: 'right',
+        lineHeight: 21,
+        fontSize: 14,
+        fontWeight: '600',
+        color: '#064BB5'
+    },
+    signInButton: {
+        width: 357,
+        gap: 10,
+        backgroundColor: '#064BB5',
+        borderRadius: 10,
+        paddingTop: 20,
+        paddingRight: 35,
+        paddingBottom: 20,
+        paddingLeft: 20,
+        fontSize: 16,
+        lineHeight: 20,
+        fontWeight: '500',
+        // alignItems:'center'
+    },
+    signInTitle: {
+        textAlign: 'center',
+        lineHeight: 30,
+        fontSize: 20,
+        fontWeight: '600',
+        color: '#FFFFFF'
+    },
+    createNewAccountTitle: {
+        textAlign: 'center',
+        lineHeight: 21,
+        fontSize: 14,
+        fontWeight: '600',
+        color: '#494949'
+    },
+    continueWithTitle: {
+        textAlign: 'center',
+        lineHeight: 21,
+        fontSize: 14,
+        fontWeight: '600',
+        color: '#064BB5'
+    },
+});
+
+export default Register;
